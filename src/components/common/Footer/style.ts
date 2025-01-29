86049533312a4e239bac55324299653647065913
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-29 21:03:45 +0900

    커밋

---
+++ src/components/common/Footer/style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 7rem;
+  display: flex;
+  background-color: #323A45;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  width: 49px;
+  height: 44px;
+  cursor: pointer;
+`
+
