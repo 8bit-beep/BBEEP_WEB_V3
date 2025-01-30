abccc1a6fefefdd44c900ed2141a4829b7e74c62
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 23:09:39 +0900

    헤더 스타일링

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
 import * as S from "./style";
 import BbeepLogo from "/assets/Logo.svg";
 
+const navItems = ["홈", "실 조회", "실 이동 관리", "결석자 조회"];
+
 const Header = () => {
+  const [activeTab, setActiveTab] = useState("홈");
+
   return (
-    <>
     <S.Container>
       <S.HeaderWrap>
         <img src={BbeepLogo} alt="로고" />
       </S.HeaderWrap>
 
       <S.NavWrap>
-        <S.NavItem>홈</S.NavItem>
-        <S.NavItem>실 조회</S.NavItem>
-        <S.NavItem>실 이동 관리</S.NavItem>
-        <S.NavItem>결석자 조회</S.NavItem>
+        {navItems.map((item) => (
+          <S.NavItem
+            key={item}
+            selected={activeTab === item}
+            onClick={() => setActiveTab(item)}
+          >
+            {item}
+          </S.NavItem>
+        ))}
       </S.NavWrap>
-
     </S.Container>
-    </>
   );
 };
 
--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -2,12 +2,12 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   width: 100%;
-  height: 71px;
-  background-color: pink;
+  height: 70px;
+  background-color: yellow;
   display: flex;
   align-items: center;
   position: relative;
-`
+`;
 
 export const HeaderWrap = styled.div`
   width: 100%;
@@ -17,18 +17,34 @@ export const HeaderWrap = styled.div`
   justify-content: space-between;
   margin-left: 130px;
   padding: 0 20px;
-`
+`;
 
 export const NavWrap = styled.div`
   display: flex;
   gap: 40px;
-  position: absolute;
+  position: relative;
   left: 50%;
   transform: translateX(-50%);
-`
+`;
 
-export const NavItem = styled.div`
+export const NavItem = styled.div<{ selected: boolean }>`
   font-size: 17px;
   font-weight: 400;
   line-height: 20px;
-`
+  cursor: pointer;
+  position: relative; /* absolute를 relative로 변경 */
+  padding-bottom: 5px;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: ${({ selected }) => (selected ? "30px" : "0")};
+    height: 2px;
+    background-color: #305B7D;
+    border-radius: 5px;
+    ${({ selected }) => !selected && "display: none"}
+  }
+`;
