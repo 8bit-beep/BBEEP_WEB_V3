c0cba5da2223132af3d9eea4ad1a2d3f2da5ab1f
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-01 17:13:31 +0900

    refactor :: useEffect 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,31 +1,38 @@
-import { useState } from "react";
-import * as S from "./style";
-import BbeepLogo from "/assets/Logo.svg";
-
-const navItems = ["홈", "실 조회", "실 이동 관리", "결석자 조회"];
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import * as S from './style';
+import Logo from '/assets/Logo.svg';
 
 const Header = () => {
-  const [activeTab, setActiveTab] = useState("홈");
+  const location = useLocation();
+  const [Item, setItem] = useState<"홈"|"실 조회"|"실 이동 관리"|"결석자 조회">("홈");
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setItem("홈");
+    } else if (location.pathname === "/class") {
+      setItem("실 조회");
+    } else if (location.pathname === "/class/move") {
+      setItem("실 이동 관리");
+    } else if (location.pathname === "/absent") {
+      setItem("결석자 조회");
+    }
+  }, [location.pathname]);
 
   return (
     <S.Container>
-      <S.HeaderWrap>
-        <img src={BbeepLogo} alt="로고" />
-      </S.HeaderWrap>
+      <S.Logo>
+        <img src={Logo} alt="logo" />
+      </S.Logo>
 
-      <S.NavWrap>
-        {navItems.map((item) => (
-          <S.NavItem
-            key={item}
-            selected={activeTab === item}
-            onClick={() => setActiveTab(item)}
-          >
-            {item}
-          </S.NavItem>
-        ))}
-      </S.NavWrap>
+      <S.Menu>
+        <S.MenuItem to="/" active={Item === "홈"}>홈</S.MenuItem>
+        <S.MenuItem to="/class" active={Item === "실 조회"}>실 조회</S.MenuItem>
+        <S.MenuItem to="/class/move" active={Item === "실 이동 관리"}>실 이동 관리</S.MenuItem>
+        <S.MenuItem to="/absent" active={Item === "결석자 조회"}>결석자 조회</S.MenuItem>
+      </S.Menu>
     </S.Container>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -1,50 +1,29 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
-  width: 100%;
-  height: 70px;
-  background-color: yellow;
+export const Container = styled.header`
+  width: 100vw;
+  height: 71px;
+  display: flex;
+  background-color: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`
+
+export const Logo = styled.div`
   display: flex;
   align-items: center;
-  position: relative;
-`;
+  padding-left: 130px;
+`
 
-export const HeaderWrap = styled.div`
-  width: 100%;
-  max-width: 1280px;
-  display: flex;
+export const Menu = styled.div`
   align-items: center;
-  justify-content: space-between;
-  margin-left: 130px;
-  padding: 0 20px;
-`;
-
-export const NavWrap = styled.div`
   display: flex;
   gap: 40px;
-  position: relative;
-  left: 50%;
-  transform: translateX(-50%);
-`;
+  margin-left: 338px;
+`
 
-export const NavItem = styled.div<{ selected: boolean }>`
+export const MenuItem = styled(Link)<{ active: boolean }>`
   font-size: 17px;
-  font-weight: 400;
-  line-height: 20px;
-  cursor: pointer;
-  position: relative; /* absolute를 relative로 변경 */
-  padding-bottom: 5px;
-
-  &:after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    width: ${({ selected }) => (selected ? "30px" : "0")};
-    height: 2px;
-    background-color: #305B7D;
-    border-radius: 5px;
-    ${({ selected }) => !selected && "display: none"}
-  }
-`;
+  text-decoration: none;
+  color: black;
+`
