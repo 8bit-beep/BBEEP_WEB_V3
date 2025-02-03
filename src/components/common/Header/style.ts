150fca80a696c4c9bb99b64ca811a35821a7e5d4
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-03 11:24:06 +0900

    fix :: 로고 유실

--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -10,38 +10,16 @@ export const Container = styled.header`
   background-color: #fff;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
   padding: 0 20px;
-`;
+`
 
 export const Logo = styled.div`
-  display: flex;
-  align-items: center;
-`;
+
+`
 
 export const Menu = styled.nav`
-  display: flex;
-  gap: 40px;
-  align-items: center;
-`;
+
+`
 
 export const MenuItem = styled(Link)<{ active: boolean }>`
-  font-size: 17px;
-  font-weight: 400;
-  text-decoration: none;
-  color: ${({ active }) => (active ? "#305B7D" : "#000")};
-  position: relative;
-  padding-bottom: 10px;
 
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: -10px;
-    left: 0;
-    width: 100%;
-    height: 3px;
-    background-color: ${({ active }) => (active ? "#305B7D" : "transparent")};
-  }
-
-  &:hover {
-    color: #305B7D;
-  }
-`;
+`
