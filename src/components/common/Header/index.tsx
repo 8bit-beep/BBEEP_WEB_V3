d797b833a795de5d3114dc2944fd19e3f958489a
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-03 12:03:24 +0900

    fix : 메뉴 아이템 클릭 시 하단 막대가 잘못 표시되는 버그 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -5,36 +5,53 @@ import Logo from '/assets/Logo.svg';
 
 const Header = () => {
   const location = useLocation();
-  const [Item, setItem] = useState<"홈"|"실 조회"|"실 이동 관리"|"결석자 조회">("홈");
+  const [activeItem, setActiveItem] = useState<"홈" | "실 조회" | "실 이동 관리" | "결석자 조회">("홈");
 
+  // 경로에 맞춰 activeItem 업데이트
   useEffect(() => {
-
     if (location.pathname === "/") {
-      setItem("홈");
+      setActiveItem("홈");
     } else if (location.pathname === "/attends") {
-      setItem("실 조회");
+      setActiveItem("실 조회");
     } else if (location.pathname === "/shifts") {
-      setItem("실 이동 관리");
-    } else if (location.pathname === "not-attend") {
-      setItem("결석자 조회");
+      setActiveItem("실 이동 관리");
+    } else if (location.pathname === "/not-attend") {
+      setActiveItem("결석자 조회");
     }
   }, [location.pathname]);
 
   return (
     <S.Container>
       <S.HeaderWrap>
-      <S.Logo>
-        <img src={Logo} alt="logo" />
-      </S.Logo>
+        <S.Logo>
+          <img src={Logo} alt="logo" />
+        </S.Logo>
 
-      <S.Menu>
-        <S.MenuItem to="/" active={Item === "홈"}>홈</S.MenuItem>
-        <S.MenuItem to="/attends" active={Item === "실 조회"}>실 조회</S.MenuItem>
-        <S.MenuItem to="/shifts" active={Item === "실 이동 관리"}>실 이동 관리</S.MenuItem>
-        <S.MenuItem to="not-attend" active={Item === "결석자 조회"}>결석자 조회</S.MenuItem>
-      </S.Menu>
+        <S.Menu>
+          <S.MenuItem 
+            to="/" 
+            active={activeItem === "홈"}>
+            홈
+          </S.MenuItem>
+          <S.MenuItem 
+            to="/attends" 
+            active={activeItem === "실 조회"}>
+            실 조회
+          </S.MenuItem>
+          <S.MenuItem 
+            to="/shifts" 
+            active={activeItem === "실 이동 관리"}>
+            실 이동 관리
+          </S.MenuItem>
+          <S.MenuItem 
+            to="not-attend" 
+            active={activeItem === "결석자 조회"}>
+            결석자 조회
+          </S.MenuItem>
+        </S.Menu>
       </S.HeaderWrap>
     </S.Container>
-)};
+  );
+};
 
-export default Header
+export default Header;
