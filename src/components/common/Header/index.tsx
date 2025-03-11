5fe5d02bdd03843b90c5733156bf39b9d47abfd1
cher1shRXD <taewoo080401@gmail.com>
2025-03-11 13:19:58 +0900

    delete: legacy codes

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -3,9 +3,12 @@ import Logo from "/assets/Logo.svg";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import ProfileDropdown from "../Dropdown/ProfileDropdown";
+import {useState} from "react";
+import {AlignJustify, X} from "lucide-react";
 
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -14,7 +17,7 @@ const Header = () => {
   };
 
   return (
-    <S.Container>
+    <S.Container $mobileMenu={mobileMenu}>
       <S.HeaderWrap>
         <S.Logo onClick={handleLogoClick} src={Logo} />
         <S.MenuWrap>
@@ -39,7 +42,33 @@ const Header = () => {
         <S.ProfileWrap>
           <ProfileDropdown />
         </S.ProfileWrap>
+        <S.MobileMenu>
+          {
+            mobileMenu ? <X onClick={() => setMobileMenu(false)} /> : <AlignJustify onClick={() => setMobileMenu(true)} />
+          }
+        </S.MobileMenu>
       </S.HeaderWrap>
+      {
+        mobileMenu && (
+          <>
+            <S.MenuItem to="/" active={location.pathname === "/"}>
+              홈
+            </S.MenuItem>
+            <S.MenuItem to="/classes" active={location.pathname === "/classes"}>
+              반별 조회
+            </S.MenuItem>
+            <S.MenuItem to="/shifts" active={location.pathname === "/shifts"}>
+              실 이동 관리
+            </S.MenuItem>
+            <S.MenuItem to="not-attend" active={location.pathname === "/not-attend"}>
+              결석자 조회
+            </S.MenuItem>
+            <S.MenuItem to="excel" active={location.pathname === "/excel"}>
+              엑셀 다운로드
+            </S.MenuItem>
+          </>
+        )
+      }
     </S.Container>
   );
 };
--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -2,41 +2,53 @@ import styled from "@emotion/styled";
 import {Link} from "react-router-dom";
 import { COLOR } from "../../../style/color/color";
 
-export const Container = styled.header`
+export const Container = styled.header<{ $mobileMenu: boolean }>`
   width: 100%;
-  height: 7rem;
+  height: ${({ $mobileMenu }) => $mobileMenu ? "32rem" : "7rem"};
+  padding: ${({ $mobileMenu }) => $mobileMenu ? "1rem" : "0"} 2rem;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: ${({ $mobileMenu }) => $mobileMenu ? "start" : "center"};
   justify-content: center;
   background-color: ${COLOR.White};
-  padding: 0 20px;
   position: fixed;
   top: 0;
   z-index: 100;
-  box-shadow: 1rem 0 0.1rem 0.001rem ${COLOR.Gray}
+  box-shadow: 1rem 0 0.1rem 0.001rem ${COLOR.Gray};
+  transition: all 0.2s;
 `;
 
 export const HeaderWrap = styled.div`
   width: 100%;
   max-width: 160rem;
-  height: 100%;
+  height: 7rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  gap: 2rem;
 `;
 
 export const Logo = styled.img`
   cursor: pointer;
+  width: 5rem;
+  margin-right: 9rem;
 `;
 
 export const Menu = styled.nav`
   height: 100%;
-  display: flex;
   gap: 2rem;
+  display: flex;
+  @media (max-width: 80rem) {
+    display: none;
+  }
 `;
 
 export const MenuWrap = styled.div`
   height: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const MenuItem = styled(Link)<{ active: boolean }>`
@@ -46,10 +58,21 @@ export const MenuItem = styled(Link)<{ active: boolean }>`
   text-decoration: none;
   color: ${COLOR.Black};
   cursor: pointer;
-  padding: 2.5rem 1rem 0 1rem;
+  padding-top: 2.5rem;
+  margin: 0 1rem;
   border-bottom: ${({ active })=> active ? "2px" : "0px"} solid ${COLOR.Main};
 `;
 
 export const ProfileWrap = styled.div`
   width: 14rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+export const MobileMenu = styled.div`
+  display: none;
+  @media (max-width: 80rem) {
+    display: flex;
+  }
 `
