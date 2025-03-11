d79f1ee461285fa51a286cb0bcce541b6946d29e
bbqcream <imground11@gmail.com>
2025-03-11 10:22:59 +0900

    feat :: 로그인 수정

--- src/components/auth/LoginForm/index.tsx
+++ src/components/auth/LoginForm/index.tsx
@@ -2,7 +2,14 @@ import * as S from "./style";
 const LoginForm = () => {
   return (
     <S.Container>
-      <S.Title>로그인 하기</S.Title>
+      <S.Logo src="/assets/Logo.svg" />
+      <S.ButtonWrap>
+        <S.Title>로그인 하기</S.Title>
+        <S.Button onClick={() => console.log("디어스로 가져라!!")}>
+          도담도담으로 로그인 <S.DodamLogo src="/assets/DodamLogo.svg" />
+        </S.Button>
+      </S.ButtonWrap>
+      <S.Blank />
     </S.Container>
   );
 };
