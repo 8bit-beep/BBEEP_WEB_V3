50ae681cfe607ad10cb58fdf1f563da6c2644746
bbqcream <imground11@gmail.com>
2025-03-11 09:21:35 +0900

    refactor :: auth 재구조

--- src/store/signup/useCodeCertified.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { CodeCertified } from "../../types/store/codeCertified";
-
-export const useCodeCertifiedStore = create<CodeCertified>((set) => ({
-  isCodeCertified: false,
-  setIsCodeCertified: (isCodeCertified: boolean) => set({ isCodeCertified }),
-}));
--- src/types/auth/signupForm.ts
+++
@@ -1,7 +0,0 @@
-export interface SignupForm {
-  email: string;
-  password: string;
-  name: string;
-  code: string;
-  isCodeCertified: boolean;
-}
