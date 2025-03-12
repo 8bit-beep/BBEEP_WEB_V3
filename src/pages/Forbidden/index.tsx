98a2bab7fea5a2e1796fd1bb7885aee8dfb33637
cher1shRXD <taewoo080401@gmail.com>
2025-03-12 16:17:58 +0900

    fix: forbidden for student page

--- src/pages/Forbidden/index.tsx
+++ src/pages/Forbidden/index.tsx
@@ -3,7 +3,9 @@ import * as S from "./style.ts";
 const Forbidden = () => {
   return (
     <S.Container>
-      <h1>403 Forbidden</h1>
+      <S.Logo src="/assets/Logo.svg" alt="로고" />
+      <S.Title>403 Forbidden :(</S.Title>
+      <S.Content>학생은 선생님용 삑을 이용할 수 없어요.</S.Content>
     </S.Container>
   );
 }
--- src/pages/Forbidden/style.ts
+++ src/pages/Forbidden/style.ts
@@ -6,4 +6,20 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 5rem;
+  font-weight: 800;
+`
+
+export const Content = styled.p`
+  font-size: 2rem;
+`
+
+export const Logo = styled.img`
+  width: 10rem;
+  height: 10rem;
 `
