90f79eef0ac8f9b75e776d026fc5aa8ce3a99711
cher1shRXD <taewoo080401@gmail.com>
2025-03-26 21:16:56 +0900

    fix: query key order

--- src/utils/getStoredOption.ts
+++ src/utils/getStoredOption.ts
@@ -1,7 +1,9 @@
+import {Option} from "../types/props/dropdownProps.ts";
+
 export const getStoredOption = (name: string) => {
   const data = localStorage.getItem(name);
   if(data) {
-    return JSON.parse(data);
+    return JSON.parse(data) as Option;
   }else{
     return;
   }
