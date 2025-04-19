86a4a159a7106f2b03cfce8339fcb2368a05d1e2
bbqcream <imground11@gmail.com>
2025-04-19 20:40:05 +0900

    Update style.ts

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -15,17 +15,17 @@ export const Map = styled.img`
 `;
 
 export const ToggleWrap = styled.div`
-    width: 18rem;
-    padding: 0.5rem 1.9375rem 0.5rem 0.5rem;
-    display: flex;
-    background: ${COLOR.White};
-    border-radius: 0.8rem;
-    position: fixed;
-    top: 8rem;
-    left: 1rem;
-    gap: 0.4rem;
-    border: 0.1rem solid ${COLOR.Main};
-`;
+  width: 18rem;
+  padding: 0.4rem;
+  display: flex;
+  background: ${COLOR.White};
+  border-radius: 0.8rem;
+  position: fixed;
+  top: 8rem;
+  left: 1rem;
+  gap: 0.4rem;
+  border: 0.1rem solid ${COLOR.Main};
+`
 
 export const ToggleItem = styled.div<{ $isFocused: boolean }>`
     flex: 1;
@@ -34,26 +34,26 @@ export const ToggleItem = styled.div<{ $isFocused: boolean }>`
     font-size: 1.6rem;
     font-weight: 600;
     color: ${({ $isFocused }) => ($isFocused ? COLOR.White : COLOR.Black)};
-    border-radius: 0.3125rem;
+    border-radius: 0.4rem;
     text-align: center;
 `;
 
 export const ConfirmAttend = styled.button`
-    width: 18rem;
-    padding: 1.6rem;
-    font-weight: 600;
-    border-radius: 0.8rem;
-    border: 0.1rem solid ${COLOR.Main};
-    background: ${COLOR.Main};
-    text-align: center;
-    position: fixed;
-    top: 8rem;
-    left: 20rem;
-    color: ${COLOR.White};
-    font-size: 1.6rem;
-    cursor: pointer;
-    &:disabled {
-        background: ${COLOR.Gray};
-        border: 0.1rem solid ${COLOR.Gray};
-    }
-`;
+  width: 18rem;
+  padding: 1.2rem;
+  border-radius: 0.8rem;
+  border: 0.1rem solid ${COLOR.Main};
+  background: ${COLOR.Main};
+  text-align: center;
+  position: fixed;
+  top: 8rem;
+  left: 20rem;
+  color: ${COLOR.White};
+  font-size: 1.6rem;
+  font-weight: 600;
+  cursor: pointer;
+  &:disabled {
+    background: ${COLOR.Gray};
+    border: 0.1rem solid ${COLOR.Gray};
+  }
+`
