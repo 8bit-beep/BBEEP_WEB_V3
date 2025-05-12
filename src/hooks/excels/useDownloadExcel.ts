dfa14468617673a2d13087d2c172d38029b387d2
김태우 <taewoo080401@gmail.com>
2025-05-12 15:14:25 +0900

    feat: api url

--- src/hooks/excels/useDownloadExcel.ts
+++ src/hooks/excels/useDownloadExcel.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const useDownloadExcel = (date: string) => {
   const getUrl = async () => {
-    const { data } = await axios.get(`/excel/presigned-url?date=${date}`);
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/excel/presigned-url?date=${date}`);
     if(data) {
       axios.get(data);
     }
