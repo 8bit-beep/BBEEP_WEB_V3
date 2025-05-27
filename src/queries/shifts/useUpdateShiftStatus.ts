6701a6952793b456f1314f0b4b3dda651afb5f81
김태우 <taewoo080401@gmail.com>
2025-05-28 08:51:31 +0900

    feat: error message

--- src/queries/shifts/useUpdateShiftStatus.ts
+++ src/queries/shifts/useUpdateShiftStatus.ts
@@ -1,5 +1,7 @@
+import { notification } from "antd";
 import bbeepAxios from "../../libs/axios/customAxios.ts";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 export const useUpdateShiftStatus = (status: string) => {
   const queryClient = useQueryClient();
@@ -14,6 +16,9 @@ export const useUpdateShiftStatus = (status: string) => {
     },
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: ["getShifts"] });
+    },
+    onError: (e) => {
+      notification.open({ message: "실이동 승인 상태 변경 실패", description: (e as AxiosError<{ message: string }>).response?.data.message});
     }
   })
 }
