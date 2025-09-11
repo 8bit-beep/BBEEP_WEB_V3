9f9d4bb673d599d2a78b3bb5178d7204fe3d13fd
bbqcream <imground11@gmail.com>
2025-09-11 11:48:52 +0900

    fix :: 빌드 오류 수정

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -1,120 +1,119 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import {
-  ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
-  REQUEST_TOKEN_KEY,
+    ACCESS_TOKEN_KEY,
+    REFRESH_TOKEN_KEY,
+    REQUEST_TOKEN_KEY,
 } from "../../constants/token/token";
 import { BaseResponse } from "../../types/response/baseResponse";
 import { TokenResponse } from "../../types/response/tokenResponse";
 import { cookie } from "../../utils/tokenStore";
 
 interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-  _retry?: boolean;
+    _retry?: boolean;
 }
 
 const bbeepAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Accept: "application/json, text/plain, */*, multipart/form-data",
-  },
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        Accept: "application/json, text/plain, */*, multipart/form-data",
+    },
+    withCredentials: true,
 });
 
 let isRefreshing = false;
 let refreshSubscribers: ((token: string) => void)[] = [];
 
 const onRefreshed = (token: string) => {
-  refreshSubscribers.forEach((callback) => callback(token));
-  refreshSubscribers = [];
+    refreshSubscribers.forEach((callback) => callback(token));
+    refreshSubscribers = [];
 };
 
 const addRefreshSubscriber = (callback: (token: string) => void) => {
-  refreshSubscribers.push(callback);
+    refreshSubscribers.push(callback);
 };
 
 bbeepAxios.interceptors.request.use(
-  async (config) => {
-    const token = cookie.get(ACCESS_TOKEN_KEY);
-    if (token) {
-      config.headers[REQUEST_TOKEN_KEY] = `Bearer ${token}`;
-    }
+    async (config) => {
+        const token = cookie.get(ACCESS_TOKEN_KEY);
+        if (token) {
+            config.headers[REQUEST_TOKEN_KEY] = `Bearer ${token}`;
+        }
 
-    if (config.data instanceof FormData) {
-      config.headers["Content-Type"] = "multipart/form-data";
-    } else {
-      config.headers["Content-Type"] = "application/json";
+        if (config.data instanceof FormData) {
+            config.headers["Content-Type"] = "multipart/form-data";
+        } else {
+            config.headers["Content-Type"] = "application/json";
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 bbeepAxios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error: AxiosError) => {
-    const originalRequest = error.config as CustomAxiosRequestConfig;
+    (response) => {
+        return response;
+    },
+    async (error: AxiosError) => {
+        const originalRequest = error.config as CustomAxiosRequestConfig;
 
-    if (originalRequest.data instanceof FormData) {
-      originalRequest.headers["Content-Type"] = "multipart/form-data";
-    } else {
-      originalRequest.headers["Content-Type"] = "application/json";
-    }
-    if (originalRequest && !originalRequest._retry) {
-      originalRequest._retry = true;
-      const refreshToken = cookie.get(REFRESH_TOKEN_KEY);
+        if (originalRequest.data instanceof FormData) {
+            originalRequest.headers["Content-Type"] = "multipart/form-data";
+        } else {
+            originalRequest.headers["Content-Type"] = "application/json";
+        }
+        if (originalRequest && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = cookie.get(REFRESH_TOKEN_KEY);
 
-      if (refreshToken) {
-        if (!isRefreshing) {
-          isRefreshing = true;
+            if (refreshToken) {
+                if (!isRefreshing) {
+                    isRefreshing = true;
 
-          try {
-            const { data } = await axios.post<BaseResponse<TokenResponse>>(
-              `${
-                import.meta.env.VITE_API_URL
-              }/auth/refresh`,
-              {
-                refreshToken
-              }
-            );
+                    try {
+                        const { data } = await axios.post<
+                            BaseResponse<TokenResponse>
+                        >(`${import.meta.env.VITE_API_URL}/auth/refresh`, {
+                            refreshToken,
+                        });
 
-            const newAccessToken = data.data.accessToken;
-            const newRefreshToken = data.data.refreshToken;
+                        const newAccessToken = data.data.data.accessToken;
+                        const newRefreshToken = data.data.data.refreshToken;
 
-            cookie.set(ACCESS_TOKEN_KEY, newAccessToken);
-            cookie.set(REFRESH_TOKEN_KEY, newRefreshToken);
+                        cookie.set(ACCESS_TOKEN_KEY, newAccessToken);
+                        cookie.set(REFRESH_TOKEN_KEY, newRefreshToken);
 
-            onRefreshed(newAccessToken);
+                        onRefreshed(newAccessToken);
 
-            originalRequest.headers[
-              REQUEST_TOKEN_KEY
-            ] = `Bearer ${newAccessToken}`;
-            return bbeepAxios(originalRequest);
-          } catch (refreshError) {
-            cookie.remove(ACCESS_TOKEN_KEY);
-            cookie.remove(REFRESH_TOKEN_KEY);
-            return Promise.reject(refreshError);
-          } finally {
-            isRefreshing = false;
-          }
+                        originalRequest.headers[
+                            REQUEST_TOKEN_KEY
+                        ] = `Bearer ${newAccessToken}`;
+                        return bbeepAxios(originalRequest);
+                    } catch (refreshError) {
+                        cookie.remove(ACCESS_TOKEN_KEY);
+                        cookie.remove(REFRESH_TOKEN_KEY);
+                        return Promise.reject(refreshError);
+                    } finally {
+                        isRefreshing = false;
+                    }
+                }
+
+                return new Promise((resolve) => {
+                    addRefreshSubscriber((newToken: string) => {
+                        originalRequest.headers[
+                            REQUEST_TOKEN_KEY
+                        ] = `Bearer ${newToken}`;
+                        resolve(bbeepAxios(originalRequest));
+                    });
+                });
+            } else {
+                return Promise.reject(error);
+            }
         }
 
-        return new Promise((resolve) => {
-          addRefreshSubscriber((newToken: string) => {
-            originalRequest.headers[REQUEST_TOKEN_KEY] = `Bearer ${newToken}`;
-            resolve(bbeepAxios(originalRequest));
-          });
-        });
-      } else {
         return Promise.reject(error);
-      }
     }
-
-    return Promise.reject(error);
-  }
 );
 
 export default bbeepAxios;
