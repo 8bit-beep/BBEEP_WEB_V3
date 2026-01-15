b0028a9c8fdb246d2302daf3f67ef2c1c9611e69
bbqcream <imground11@gmail.com>
2026-01-15 15:35:08 +0900

    fix: 사이드바 새로고침 notification 생성

--- src/components/common/layouts/Sidebar.tsx
+++ src/components/common/layouts/Sidebar.tsx
@@ -9,100 +9,116 @@ import { useGetAttendApproveOneQuery } from "../../../queries/attendApprove/getA
 import { COLOR } from "../../../style/color/color.ts";
 import Skeleton from "../Skeleton.tsx";
 import { useAttendStatusIdxByTime } from "../../../hooks/attends/useAttendStatusIdxByTime.ts";
+import { notification } from "antd";
 
 const Sidebar = () => {
-  const statusIdx = useAttendStatusIdxByTime();
-  const { sidebarData, setSidebarData } = useSidebarDataStore();
-  const { data: selfstudy, isLoading: selfstudyLoading, refetch: selfstudyRefetch } = useGetAttendsByRoom(
-    sidebarData,
-    "WINTER_CAMP_SELF_STUDY"
-  );
-  const { data: lecture, isLoading: lectureLoading, refetch: lectureRefetch } = useGetAttendsByRoom(
-    sidebarData,
-    "WINTER_CAMP_LECTURE"
-  );
-  const data = [...selfstudy, ...lecture];
-  const isLoading = selfstudyLoading || lectureLoading;
-  const refetch = async () => {
-    await selfstudyRefetch();
-    await lectureRefetch();
-  };
+    const statusIdx = useAttendStatusIdxByTime();
+    const { sidebarData, setSidebarData } = useSidebarDataStore();
+    const {
+        data: selfstudy,
+        isLoading: selfstudyLoading,
+        refetch: selfstudyRefetch,
+    } = useGetAttendsByRoom(sidebarData, "WINTER_CAMP_SELF_STUDY");
+    const {
+        data: lecture,
+        isLoading: lectureLoading,
+        refetch: lectureRefetch,
+    } = useGetAttendsByRoom(sidebarData, "WINTER_CAMP_LECTURE");
+    const data = [...selfstudy, ...lecture];
+    const isLoading = selfstudyLoading || lectureLoading;
+    const refetch = async () => {
+        await selfstudyRefetch();
+        await lectureRefetch();
+        notification.open({
+            message: "새로고침 되었습니다.",
+        });
+    };
 
-  const attendedCount = data.filter((attend) => attend.statuses[statusIdx].status !== "NOT_ATTEND").length;
+    const attendedCount = data.filter(
+        (attend) => attend.statuses[statusIdx].status !== "NOT_ATTEND"
+    ).length;
 
-  const { mutate } = useApproveAttend(sidebarData);
-  const { data: approve } = useGetAttendApproveOneQuery(sidebarData);
-  const location = useLocation();
+    const { mutate } = useApproveAttend(sidebarData);
+    const { data: approve } = useGetAttendApproveOneQuery(sidebarData);
+    const location = useLocation();
 
-  useEffect(() => {
-    setSidebarData(null);
-  }, [location.pathname, setSidebarData]);
+    useEffect(() => {
+        setSidebarData(null);
+    }, [location.pathname, setSidebarData]);
 
-  
-
-  return (
-    <div className="w-full h-screen flex flex-col gap-3 bg-white pt-13 pb-36 px-4 z-10">
-      {/* sidebar header */}
-      <div className="w-full flex items-center gap-4">
-        {/* 현재 실 */}
-        <p className="text-2xl font-semibold flex-1">
-          {parseRoomName(sidebarData || "NOTFOUND")} 인원
-        </p>
-        {/* 승인버튼 wrap*/}
-        <div className="flex gap-1">
-          <button
-            className="py-1 px-3 rounded-xl text-white border-none text-xs cursor-pointer"
-            style={{
-              background: approve?.approvedTeacher ? COLOR.Red : COLOR.Main,
-            }}
-            onClick={() => {
-              mutate();
-            }}
-          >
-            {approve?.approvedTeacher ? "전체 승인취소" : "전체 승인하기"}
-          </button>
-          {/* 새로고침 버튼 */}
-          <div
-            className="py-1 px-3 text-main bg-gray rounded-xl transition-all duration-[0.3s] text-xs"
-            onClick={() => refetch()}
-          >
-            새로고침
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex-1 min-h-0 flex flex-col gap-3 bg-gray pt-3 pb-24 px-4 overflow-y-auto rounded-xl">
-        {!isLoading && (
-          <div className="w-fit bg-main rounded-xl text-sm font-normal px-3 py-2 text-white">
-            인원 {attendedCount}/{data.length}명
-          </div>
-        )}
-        {/* 학생들 나오는 곳 */}
-        {isLoading ? (
-          Array.from({ length: 4 }).map((_, idx) => (
-            <div className="w-full mb-4" key={idx}>
-              <Skeleton width="100%" height="5rem" borderRadius="0.8rem" />
+    return (
+        <div className="w-full h-screen flex flex-col gap-3 bg-white pt-13 pb-36 px-4 z-10">
+            {/* sidebar header */}
+            <div className="w-full flex items-center gap-4">
+                {/* 현재 실 */}
+                <p className="text-2xl font-semibold flex-1">
+                    {parseRoomName(sidebarData || "NOTFOUND")} 인원
+                </p>
+                {/* 승인버튼 wrap*/}
+                <div className="flex gap-1">
+                    <button
+                        className="py-1 px-3 rounded-xl text-white border-none text-xs cursor-pointer"
+                        style={{
+                            background: approve?.approvedTeacher
+                                ? COLOR.Red
+                                : COLOR.Main,
+                        }}
+                        onClick={() => {
+                            mutate();
+                        }}
+                    >
+                        {approve?.approvedTeacher
+                            ? "전체 승인취소"
+                            : "전체 승인하기"}
+                    </button>
+                    {/* 새로고침 버튼 */}
+                    <div
+                        className="py-1 px-3 text-main bg-gray rounded-xl transition-all duration-[0.3s] text-xs cursor-pointer"
+                        onClick={() => refetch()}
+                    >
+                        새로고침
+                    </div>
+                </div>
             </div>
-          ))
-        ) : data && data.length > 0 ? (
-          data
-            .sort((a, b) => Number(a.studentId) - Number(b.studentId))
-            .map((item) => (
-              <div className="w-full mb-4" key={item.studentId}>
-                <AttendStudent
-                  data={item}
-                  room={sidebarData || "NOTFOUND"}
-                  refetch={refetch}
-                />
-              </div>
-            ))
-        ) : (
-          <div className="w-full h-5 flex justify-center items-center text-xl text-gray">
-            출석한 인원이 없습니다.
-          </div>
-        )}
-      </div>
-    </div>
-  );
+            <div className="w-full flex-1 min-h-0 flex flex-col gap-3 bg-gray pt-3 pb-24 px-4 overflow-y-auto rounded-xl">
+                {!isLoading && (
+                    <div className="w-fit bg-main rounded-xl text-sm font-normal px-3 py-2 text-white">
+                        인원 {attendedCount}/{data.length}명
+                    </div>
+                )}
+                {/* 학생들 나오는 곳 */}
+                {isLoading ? (
+                    Array.from({ length: 4 }).map((_, idx) => (
+                        <div className="w-full mb-4" key={idx}>
+                            <Skeleton
+                                width="100%"
+                                height="5rem"
+                                borderRadius="0.8rem"
+                            />
+                        </div>
+                    ))
+                ) : data && data.length > 0 ? (
+                    data
+                        .sort(
+                            (a, b) => Number(a.studentId) - Number(b.studentId)
+                        )
+                        .map((item) => (
+                            <div className="w-full mb-4" key={item.studentId}>
+                                <AttendStudent
+                                    data={item}
+                                    room={sidebarData || "NOTFOUND"}
+                                    refetch={refetch}
+                                />
+                            </div>
+                        ))
+                ) : (
+                    <div className="w-full h-5 flex justify-center items-center text-xl text-gray">
+                        출석한 인원이 없습니다.
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
